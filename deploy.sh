a9c52987f3ec86f915622cea09b18edbfca32105
Kaustubh <kaustubhagrawal8838@gmail.com>
2021-12-31 07:10:25 +0530

    setup github pages

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# add .nojekyll to bypass GitHub Page's default behavior
+touch .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:kaustubh-2406/kaustubh-2406.github.io.git main
